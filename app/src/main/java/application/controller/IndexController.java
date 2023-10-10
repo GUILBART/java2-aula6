f11310705c3536ba5c37c455cae71b59ac8a9210
Lennon Machado <lennon2049@gmail.com>
2023-10-10 19:04:06 +0000

    Segunda Parte

--- app/src/main/java/application/controller/IndexController.java
+++ app/src/main/java/application/controller/IndexController.java
@@ -2,12 +2,16 @@ package application.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
     @RequestMapping("/")
-    public String home() {
+    public String home(
+            Model model,
+            @RequestParam("nome") String pNome) {
+        model.addAttribute("nome", pNome);
         return "home";
     }
 }
-
