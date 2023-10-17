a01734a2df9a5bd05fa8a97207817f8db8728011
Guilhereme maçanaro <guilhermevaldeir85@gmail.com>
2023-10-17 19:45:42 +0000

    finalizada

--- app/src/main/java/application/model/Aluno.java
+++ app/src/main/java/application/model/Aluno.java
@@ -1,34 +1,47 @@
 package application.model;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-
+@Entity
+@Table(name = "alunos")
 public class Aluno {
     private int id;
     private String nome;
     private int idade;
 
-    
-}
-public int getId(){
-    return this.id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-}
-public void setId(){
-    this.id = id;
+    public int getId() {
+        return this.id;
 
-}
-public String getNome(){
-    return this.nome;
+    }
 
-}
-public void setNome (String nome){
-    this.nome = nome;
+    public void setId(int id) {
+        this.id = id;
 
-}
-public int getIdade(){
-    return this.idade;
+    }
 
-}
-public void setIdade(int idade){
-    this.idade = idade;
+    public String getNome() {
+        return this.nome;
+
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+
+    }
+
+    public int getIdade() {
+        return this.idade;
+
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
